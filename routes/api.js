7a4f474371eae7f3c2024cfa7dfc6eb314983f05
JanNDen <jc@pntr.eu>
2021-02-26 10:56:09 +0200

    Route /api/translate

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,26 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
+  app.route("/api/translate").post((req, res) => {
+    // Initialize variables
+    const { text, locale } = req;
+
+    // Verify variables
+    if (!text || !locale)
+      return res.json({ error: "Required field(s) missing" });
+    if (text === "") return res.json({ error: "No text to translate" });
+    if (locale !== "american-to-british" && locale !== "british-to-american")
+      return res.json({ error: "Invalid value for locale field" });
+
+    // Proceed to translation
+    translator.translate(text, locale);
+    return res.json({
+      text: translator.text,
+      translation: translator.translation,
     });
+  });
 };
