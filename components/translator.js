99782543ed7b4025e7a9c123c028eebfaaab9214
JanNDen <jc@pntr.eu>
2021-02-26 11:21:47 +0200

    Reverse Spelling and Titles

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,19 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
-class Translator {
+// Reverse Spelling and Titles
+const reverseObject = (obj) => {
+  return Object.entries(obj).reduce((acc, entry) => {
+    const [key, value] = entry;
+    acc[value] = key;
+    return acc;
+  }, {});
+};
+const britishToAmericanSpelling = reverseObject(americanToBritishSpelling);
+const britishToAmericanTitles = reverseObject(americanToBritishTitles);
 
-}
+class Translator {}
 
 module.exports = Translator;
